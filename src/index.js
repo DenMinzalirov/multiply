aca8a252b52db95fbcdd7d549e5027906af3ffb8
Den Minzakirov <Den.Minzakirov@gmail.com>
2019-10-01 22:08:42 +0300

    init: inpliment multiply()

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function multiply(first, second) {
   // your solution
+  let temp1 = BigInt(first) ;
+  let temp2 = BigInt(second);
+  return (temp1*temp2).toString();
 }
+
+// function multiply(first, second) {
+//   let temp1 = Number(first) ;
+//   let temp2 = Number(second);
+//   // console.log('fn');
+//   // your solution
+//   return console.log((temp1*temp2).toString());
+// }
+// multiply('2','50');
